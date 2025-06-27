c558d0f0fb8532663589da9dba472aef59f842bb
vaishnavi-reddy <svrvaishnavi26@gmail.com>
2025-06-27 21:54:04 +0530

    adding 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+void countWordFrequencies(string &s,int n)
+{
+    map<string,int>mpp;
+    string word;
+    int i=0;
+    for(int j=0;j<n;j++)
+    {
+        if(s[j]==' ')
+        {
+            word=s.substr(i,j-i);
+            mpp[word]++;
+            i=j+1;
+        }
+        if(j==n-1)
+        {
+            word=s.substr(i,j-i+1);
+            mpp[word]++;
+        }
+    }
+    for(auto it:mpp)
+    {
+        cout<<it.first<<":"<<it.second<<"\n";
+    }
+}
+int main()
+{
+    string s;
+    cout<<"Enter a sentence: ";
+    getline(cin,s);
+    int n=s.size();
+    countWordFrequencies(s,n);
+} 
